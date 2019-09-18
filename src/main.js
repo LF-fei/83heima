6a2fd9cdc185c268943fdf1a48ef8c6441e9642c
LF-fei helper = manager <1129661235@qq.com>
2019-09-18 23:04:02 +0800

    黑马头条PC-登录模块-安装axios-配置全局使用

--- src/main.js
+++ src/main.js
@@ -4,9 +4,12 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import './assets/index.less'
+import axios from 'axios'
+
 Vue.use(ElementUI)
 Vue.config.productionTip = false
-
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
+Vue.proyotype.$http = axios
 new Vue({
   router,
   render: h => h(App)
