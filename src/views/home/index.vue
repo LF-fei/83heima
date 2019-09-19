f4571da24860be9b5377497ece6e3612d4480f15
LF-fei helper = manager <1129661235@qq.com>
2019-09-19 14:09:11 +0800

    "黑马头条PC-主页模块-页面布局"

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,10 +1,26 @@
 <template>
-  <div>321</div>
+  <el-container>
+    <el-aside>
+       左侧内容
+    </el-aside>
+      <el-container>
+        <el-header>头部</el-header>
+          <el-main>容器
+            <!-- 二级容器 -->
+          <router-view></router-view>
+          </el-main>
+      </el-container>
+
+  </el-container>
+
 </template>
 
 <script>
-export default {
 
+export default {
+  components: {
+
+  }
 }
 </script>
 
