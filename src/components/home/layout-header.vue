2d3cb190a9735e1548e2ba1ff81481e80b80356a
LF-fei helper = manager <1129661235@qq.com>
2019-09-20 18:57:52 +0800

    黑马头条PC-主页模块-头部结构及下拉菜单

---
+++ src/components/home/layout-header.vue
@@ -0,0 +1,48 @@
+<template>
+  <el-row type="flex" justify="space-between" class="layout-header">
+    <el-col :span="6" style="margin-top:10px">
+        <i class="el-icon-s-unfold"></i>
+        <span class="title">江苏传智播客教育科股份有限公司</span></el-col>
+    <el-col :span="4">
+        <img src="../../assets/avatar.jpg"  class="layout-img" alt="">
+         <el-dropdown trigger="click">
+       <span class="el-dropdown-link">
+        小仙女<i class="el-icon-arrow-down el-icon--right"></i>
+      </span>
+      <el-dropdown-menu slot="dropdown">
+        <el-dropdown-item icon="el-icon-s-check">个人信息</el-dropdown-item>
+        <el-dropdown-item icon="el-icon-s-promotion">git地址</el-dropdown-item>
+        <el-dropdown-item icon="el-icon-headset">兴趣爱好</el-dropdown-item>
+          <el-dropdown-item icon="el-icon-switch-button">退出</el-dropdown-item>
+      </el-dropdown-menu>
+    </el-dropdown>
+    </el-col>
+
+  </el-row>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+.layout-header{
+    height: 50px;
+    .el-icon-s-unfold{
+         font-size: 22px;
+    }
+    .title{
+        vertical-align: top;
+        margin-left:5px;
+    }
+    .layout-img{
+    height: 40px;
+    width: 40px;
+    border-radius: 50%;
+    margin-right: 7px;
+    vertical-align: middle;
+}
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,10 +1,13 @@
 <template>
   <el-container>
-    <el-aside>
+    <el-aside style="width:200px;overflow:hidden;background-color:#323745">
         <layout-left></layout-left>
     </el-aside>
       <el-container>
-        <el-header>头部</el-header>
+        <el-header>
+          <layout-header></layout-header>
+
+        </el-header>
           <el-main>容器
             <!-- 二级容器 -->
           <router-view></router-view>
@@ -17,9 +20,11 @@
 
 <script>
 import layoutLeft from '../../components/home/layout-left'
+import layoutHeader from '../../components/home/layout-header'
 export default {
   components: {
-    'layout-left': layoutLeft
+    'layout-left': layoutLeft,
+    'layout-header': layoutHeader
   }
 }
 </script>
