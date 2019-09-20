96ffa4ddcbe1274c433ae7ceb3d419f3a8b4d494
LF-fei helper = manager <1129661235@qq.com>
2019-09-20 21:19:28 +0800

    黑马头条PC-主页模块-头部退出系统

--- src/components/home/layout-header.vue
+++ src/components/home/layout-header.vue
@@ -5,15 +5,14 @@
         <span class="title">江苏传智播客教育科股份有限公司</span></el-col>
     <el-col :span="4">
         <img :src="user.photo ? user.photo  : userImg "  class="layout-img" alt="">
-         <el-dropdown trigger="click">
+         <el-dropdown trigger="click" @command="CommandClick">
        <span class="el-dropdown-link">
         {{user.name}}<i class="el-icon-arrow-down el-icon--right"></i>
       </span>
-      <el-dropdown-menu slot="dropdown">
-        <el-dropdown-item icon="el-icon-s-check">个人信息</el-dropdown-item>
-        <el-dropdown-item icon="el-icon-s-promotion">git地址</el-dropdown-item>
-        <el-dropdown-item icon="el-icon-headset">兴趣爱好</el-dropdown-item>
-          <el-dropdown-item icon="el-icon-switch-button">退出</el-dropdown-item>
+      <el-dropdown-menu slot="dropdown" >
+        <el-dropdown-item icon="el-icon-s-check" command="account">个人信息</el-dropdown-item>
+        <el-dropdown-item icon="el-icon-s-promotion" command="git">git地址</el-dropdown-item>
+          <el-dropdown-item icon="el-icon-switch-button" command="dropout">退出</el-dropdown-item>
       </el-dropdown-menu>
     </el-dropdown>
     </el-col>
@@ -38,11 +37,22 @@ export default {
       }).then(result => {
         this.user = result.data.data
       })
+    },
+    CommandClick (key) {
+      if (key === 'account') {
+        console.log(1)
+      } else if (key === 'git') {
+        window.location.href = 'http://www.baidu.com'
+      } else {
+        window.localStorage.clear()
+        this.$router.push('/login')
+      }
     }
   },
   created () {
     this.getUser()
   }
+
 }
 </script>
 
