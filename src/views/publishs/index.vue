06a3f3526238dc1fe8e81bacff04045d0af994c2
LF-fei helper = manager <1129661235@qq.com>
2019-09-24 21:24:51 +0800

    黑马头条PC-发表文章-发布文章-简单发布文章

--- src/views/publishs/index.vue
+++ src/views/publishs/index.vue
@@ -1,35 +1,40 @@
 <template>
-    <el-card>
-         <bread-crumb slot='header'>
-          <template slot='title'>发布文章</template>
-       </bread-crumb>
-       <!-- 表单 -->
-       <el-form style='margin-left:100px' label-width="100px">
-            <el-form-item label="标题">
-                <el-input style='width:400px'></el-input>
-            </el-form-item>
-            <el-form-item label="内容">
-                <el-input  type="textarea" :rows="4"  placeholder="请输入内容"></el-input>
-            </el-form-item>
-            <el-form-item label="封面">
-                <el-radio-group>
-                    <el-radio>单图</el-radio>
-                    <el-radio>三图</el-radio>
-                    <el-radio>无图</el-radio>
-                    <el-radio>自动</el-radio>
-                </el-radio-group>
-            </el-form-item>
-            <el-form-item label="频道">
-                <el-select>
-                    <el-option v-for=' item in channels' :key="item.id" :value="item.id" :label="item.name"></el-option>
-                </el-select>
-            </el-form-item>
-            <el-form-item>
-                <el-button type='primary'>发布文章</el-button>
-                <el-button>存入草稿</el-button>
-            </el-form-item>
-
-       </el-form>
+  <el-card>
+    <bread-crumb slot="header">
+      <template slot="title">发布文章</template>
+    </bread-crumb>
+    <!-- 表单 -->
+    <el-form ref='publishForm' :model="formData" :rules="rulesData" style="margin-left:100px" label-width="100px">
+      <el-form-item prop="title" label="标题">
+        <el-input v-model="formData.title" style="width:400px"></el-input>
+      </el-form-item>
+      <el-form-item prop="content" label="内容">
+        <el-input v-model="formData.content" type="textarea" :rows="4" placeholder="请输入内容"></el-input>
+      </el-form-item>
+      <el-form-item prop='cover' label="封面">
+        <el-radio-group v-model='formData.cover.type'>
+          <el-radio :label='1'>单图</el-radio>
+          <el-radio :label='3'>三图</el-radio>
+          <el-radio :label='0'>无图</el-radio>
+          <el-radio :label='-1'>自动</el-radio>
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item prop="channel_id" label="频道">
+        <el-select  v-model='formData.channel_id'>
+          <el-option
+            :model="formData.channel_id"
+            v-for=" item in channels"
+            :key="item.id"
+            :value="item.id"
+            :label="item.name"
+          ></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button @click="publish" type="primary">发布文章</el-button>
+        <el-button>存入草稿</el-button>
+      </el-form-item>
+    </el-form>
   </el-card>
 </template>
 
@@ -37,10 +42,41 @@
 export default {
   data () {
     return {
-      channels: []
+      channels: [],
+      formData: {
+        title: '',
+        content: '',
+        channel_id: null,
+        cover: {
+          type: 0,
+          images: []
+        }
+      },
+      // 校验规则
+      rulesData: {
+        title: [{ required: true, message: '标题不能为空' }, { min: 5, max: 30, message: '标题长度在5-30个字符' }],
+        content: [{ required: true, message: '内容不能为空' }],
+        channel_id: [{ required: true, message: '频道不能为空' }]
+      }
     }
   },
   methods: {
+    //   发布文章
+    publish () {
+      this.$refs.publishForm.validate((isOk) => {
+        if (isOk) {
+          this.$axios({
+            url: '/articles',
+            method: 'post',
+            data: this.formData,
+            params: { draft: false }
+          }).then(() => {
+            this.$router.push('/home/articles')
+          })
+        }
+      })
+    },
+    // 获取有道数据
     getChannels () {
       this.$axios({
         url: '/channels'
@@ -56,5 +92,4 @@ export default {
 </script>
 
 <style>
-
 </style>
