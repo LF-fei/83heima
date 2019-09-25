9a84507434e475951ae85820ab85bf8c3b787625
LF-fei helper = manager <1129661235@qq.com>
2019-09-25 20:40:12 +0800

    黑马头条PC-账户信息-数据加载给表单

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -1,13 +1,59 @@
 <template>
-  <div>123</div>
+   <el-card>
+       <bread-crumb slot="header">
+         <template slot="title">账户信息</template>
+       </bread-crumb>
+       <el-form>
+           <el-form-item  label="用户名称" label-width="100px">
+               <el-input v-model="formData.name" style="width:300px"></el-input>
+           </el-form-item>
+            <el-form-item  label="简介" label-width="100px">
+               <el-input v-model="formData.intro" style="width:300px"></el-input>
+           </el-form-item>
+            <el-form-item label="邮箱" label-width="100px">
+               <el-input v-model="formData.email" style="width:300px"></el-input>
+           </el-form-item>
+            <el-form-item label="手机号" label-width="100px">
+               <el-input disabled v-model="formData.mobile" style="width:300px"></el-input>
+           </el-form-item>
+           <el-form-item>
+              <el-button type='primary'>保存信息</el-button>
+          </el-form-item>
+       </el-form>
+        <img class="head-img" :src="formData.photo ? formData.photo :defaultImg" alt="">
+   </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      defaultImg: require('../../assets/avatar.jpg'),
+      formData: {}
+    }
+  },
+  methods: {
+    getUser () {
+      this.$axios({
+        url: '/user/profile'
+      }).then(result => {
+        this.formData = result.data
+      })
+    }
+  },
+  created () {
+    this.getUser()
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+ .head-img {
+      width: 200px;
+      height: 200px;
+      border-radius: 50%;
+      position: absolute;
+      top:120px;
+      right:400px;
+  }
 </style>
